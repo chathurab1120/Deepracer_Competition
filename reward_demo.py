ef0c5dbb14818d7b053d01951d6f744802c85c17
Chathura Brahakmanage <60991570+chathurab1120@users.noreply.github.com>
2024-02-01 14:00:18 -0800

    Add files via upload

---
+++ reward_demo.py
@@ -0,0 +1,172 @@
+# ==========================================================
+# ==========================================================
+# DeepRacer Reward Function
+# - AWS Training script
+#
+# ==========================================================
+# ==========================================================
+
+
+# ----------------------------------------------------------
+# AWS Default Reward Function
+# - Uses a single action: follow the center line
+# ----------------------------------------------------------
+
+def reward_function(params):
+    '''
+    Example of rewarding the agent to follow center line
+    '''
+
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+
+    return float(reward)
+
+
+def reward_function2(params):
+    '''
+    Example of rewarding the agent to stay inside the two borders of the track
+    '''
+
+    # Read input parameters
+    all_wheels_on_track = params['all_wheels_on_track']
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+
+    # Give a very low reward by default
+    reward = 1e-3
+
+    # Give a high reward if no wheels go off the track and
+    # the agent is somewhere in between the track borders
+    if all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05:
+        reward = 1.0
+
+    # Always return a float value
+    return float(reward)
+
+def reward_function3(params):
+    '''
+    Example of penalize steering, which helps mitigate zig-zag behaviors
+    '''
+
+    # Read input parameters
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+    steering = abs(params['steering_angle']) # Only need the absolute steering angle
+
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+
+    # Give higher reward if the agent is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+
+    # Steering penality threshold, change the number based on your action space setting
+    ABS_STEERING_THRESHOLD = 15
+
+    # Penalize reward if the agent is steering too much
+    if steering > ABS_STEERING_THRESHOLD:
+        reward *= 0.8
+
+    return float(reward)
+
+def reward_function4(params):
+    '''
+    Example of rewarding the agent to stay inside two borders
+    and penalizing getting too close to the objects in front
+    '''
+
+    all_wheels_on_track = params['all_wheels_on_track']
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+    objects_distance = params['objects_distance']
+    _, next_object_index = params['closest_objects']
+    objects_left_of_center = params['objects_left_of_center']
+    is_left_of_center = params['is_left_of_center']
+
+    # Initialize reward with a small number but not zero
+    # because zero means off-track or crashed
+    reward = 1e-3
+
+    # Reward if the agent stays inside the two borders of the track
+    if all_wheels_on_track and (0.5 * track_width - distance_from_center) >= 0.05:
+        reward_lane = 1.0
+    else:
+        reward_lane = 1e-3
+
+    # Penalize if the agent is too close to the next object
+    reward_avoid = 1.0
+
+    # Distance to the next object
+    distance_closest_object = objects_distance[next_object_index]
+    # Decide if the agent and the next object is on the same lane
+    is_same_lane = objects_left_of_center[next_object_index] == is_left_of_center
+
+    if is_same_lane:
+        if 0.5 <= distance_closest_object < 0.8:
+            reward_avoid *= 0.5
+        elif 0.3 <= distance_closest_object < 0.5:
+            reward_avoid *= 0.2
+        elif distance_closest_object < 0.3:
+            reward_avoid = 1e-3 # Likely crashed
+
+    # Calculate reward by putting different weights on
+    # the two aspects above
+    reward += 1.0 * reward_lane + 4.0 * reward_avoid
+
+    return reward
+
+
+# ----------------------------------------------------------
+# All DeepRacer Parameters
+# ----------------------------------------------------------
+
+deepracer_params = {
+    "all_wheels_on_track": Boolean,        # flag to indicate if the agent is on the track
+    "x": float,                            # agent's x-coordinate in meters
+    "y": float,                            # agent's y-coordinate in meters
+    "closest_objects": [int, int],         # zero-based indices of the two closest objects to the agent's current position of (x, y).
+    "closest_waypoints": [int, int],       # indices of the two nearest waypoints.
+    "distance_from_center": float,         # distance in meters from the track center
+    "is_crashed": Boolean,                 # Boolean flag to indicate whether the agent has crashed.
+    "is_left_of_center": Boolean,          # Flag to indicate if the agent is on the left side to the track center or not.
+    "is_offtrack": Boolean,                # Boolean flag to indicate whether the agent has gone off track.
+    "is_reversed": Boolean,                # flag to indicate if the agent is driving clockwise (True) or counter clockwise (False).
+    "heading": float,                      # agent's yaw in degrees
+    "objects_distance": [float, ],         # list of the objects' distances in meters between 0 and track_length in relation to the starting line.
+    "objects_heading": [float, ],          # list of the objects' headings in degrees between -180 and 180.
+    "objects_left_of_center": [Boolean, ], # list of Boolean flags indicating whether elements' objects are left of the center (True) or not (False).
+    "objects_location": [(float, float),], # list of object locations [(x,y), ...].
+    "objects_speed": [float, ],            # list of the objects' speeds in meters per second.
+    "progress": float,                     # percentage of track completed
+    "speed": float,                        # agent's speed in meters per second (m/s)
+    "steering_angle": float,               # agent's steering angle in degrees
+    "steps": int,                          # number steps completed
+    "track_length": float,                 # track length in meters.
+    "track_width": float,                  # width of the track
+    "waypoints": [(float, float), ]        # list of (x,y) as milestones along the track center
+}
